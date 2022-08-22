ff4ed983a71144370dddf190676644ac19f5991f
macloujulian <65403593+macloujulian@users.noreply.github.com>
2022-08-22 17:19:48 -0300

    Update douserdata2.sh

--- slavejenkins/douserdata2.sh
+++ slavejenkins/douserdata2.sh
@@ -32,4 +32,4 @@ cp /root/.ssh/authorized_keys /var/jenkins_home/.ssh/authorized_keys
 chmod 700 /var/jenkins_home/.ssh
 chmod 600 /var/jenkins_home/.ssh/authorized_keys
 chown -R 1000:1000 /var/jenkins_home
-docker run -p 2222:22 -v $PWD/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart always -d macloujulian/slavejenkins
+docker run -p 2222:22 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart always -d macloujulian/slavejenkins
