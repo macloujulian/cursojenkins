abe7bf4973aa621e3851d0ec0d834d6c1f5fe209
macloujulian <65403593+macloujulian@users.noreply.github.com>
2022-08-22 16:48:51 -0300

    Create douserdata2.sh

---
+++ slavejenkins/douserdata2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# install dependencies
+apt-get update
+
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# get gpg key
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# setup repository
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# update repository
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+systemctl enable docker.service
+systemctl enable containerd.service
+
+# jenkins setup
+mkdir -p /var/jenkins_home/.ssh
+cp /root/.ssh/authorized_keys /var/jenkins_home/.ssh/authorized_keys
+chmod 700 /var/jenkins_home/.ssh
+chmod 600 /var/jenkins_home/.ssh/authorized_keys
+chown -R 1000:1000 /var/jenkins_home
+docker run -p 2222:22 -v $PWD/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart always -d macloujulian/slavejenkins
