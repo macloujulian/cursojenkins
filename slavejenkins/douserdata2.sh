2f409166b63aec6ce7e5239a7893f902f91b23f4
macloujulian <65403593+macloujulian@users.noreply.github.com>
2022-08-23 10:51:53 -0300

    Update douserdata2.sh

--- slavejenkins/douserdata2.sh
+++ slavejenkins/douserdata2.sh
@@ -30,3 +30,10 @@ systemctl enable containerd.service
 
 docker run -d --rm --name=agent1 -p 2222:22 \
 jenkins/ssh-agent:alpine
+
+# docker container update
+
+VARS1="HOME=|USER=|MAIL=|LC_ALL=|LS_COLORS=|LANG="
+VARS2="HOSTNAME=|PWD=|TERM=|SHLVL=|LANGUAGE=|_="
+VARS="${VARS1}|${VARS2}"
+docker exec agent1 sh -c "env | egrep -v '^(${VARS})' >> /etc/environment"
